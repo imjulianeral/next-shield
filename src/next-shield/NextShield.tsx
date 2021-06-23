4589aeb90f915d239285a8097e3798a9d38a1819
Jorge Acero <julianacero98@gmail.com>
2021-06-23 00:46:59 -0500

    eslint and docs added

--- src/next-shield/NextShield.tsx
+++ src/next-shield/NextShield.tsx
@@ -1,20 +1,32 @@
-import { NextRouter } from 'next/router'
-import { FC, useEffect } from 'react'
+import type { NextRouter } from 'next/router'
+import type { FC } from 'react'
+import { useEffect } from 'react'
 
-interface Props {
+export interface NextShieldProps {
+  /** Piece of state which indicates if the user is authenticated or not */
   isAuth: boolean
+
   isLoading: boolean
   router: NextRouter
+  loginRoute: string
+  accessRoute: string
   privateRoutes: string[]
   publicRoutes: string[]
   hybridRoutes?: string[]
   LoadingComponent: FC
 }
 
-export const NextShield: FC<Props> = ({
+/**
+ * 😉 Component designed to protect the routes of your app.
+ * @param props - The {@link NextShieldProps | parameters} that you must put when using NextShield
+ */
+
+export const NextShield: FC<NextShieldProps> = ({
   isAuth,
   isLoading,
   router,
+  loginRoute,
+  accessRoute,
   privateRoutes,
   publicRoutes,
   hybridRoutes,
@@ -26,8 +38,8 @@ export const NextShield: FC<Props> = ({
   const pathIsHybrid = hybridRoutes?.indexOf(router.pathname) !== -1
 
   useEffect(() => {
-    if (!isAuth && !isLoading && pathIsProtected) router.replace('/')
-    if (isAuth && !isLoading && pathIsPublic) router.replace('/protected')
+    if (!isAuth && !isLoading && pathIsProtected) router.replace(loginRoute)
+    if (isAuth && !isLoading && pathIsPublic) router.replace(accessRoute)
   }, [router, isAuth, isLoading, pathIsProtected, pathIsPublic])
 
   if (
