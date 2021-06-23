00dda231cee4c3a1b110efc1e2686b37e96c67c0
Jorge Acero <julianacero98@gmail.com>
2021-06-22 19:28:56 -0500

    unexpected token error fixed

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,2 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.NextShield = void 0;
-var NextShield_1 = require("./NextShield");
-Object.defineProperty(exports, "NextShield", { enumerable: true, get: function () { return NextShield_1.NextShield; } });
+export { NextShield } from './NextShield';
+//# sourceMappingURL=index.js.map
