79172b878c7c88fb0d913d78c2ea300b17f5f547
Jorge Acero <julianacero98@gmail.com>
2021-06-22 19:37:17 -0500

    export syntax changed

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,6 @@
-export { NextShield } from './NextShield';
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.NextShield = void 0;
+var NextShield_1 = require("./NextShield");
+Object.defineProperty(exports, "NextShield", { enumerable: true, get: function () { return NextShield_1.NextShield; } });
 //# sourceMappingURL=index.js.map
