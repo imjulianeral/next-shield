1f03e754fce679e4c77002ab98cc60405bf4392e
panduwijayasaputra <panduwijaya.dev@gmail.com>
2022-06-13 01:27:58 +0700

    test

--- src/components/NextShield.tsx
+++ src/components/NextShield.tsx
@@ -60,7 +60,7 @@ export function NextShield<
   const pathIsAuthorized =
     RBAC && userRole && verifyPath(RBAC[userRole].grantedRoutes, pathname)
   const access = getAccessRoute(RBAC, userRole, accessRoute)
-
+  console.log('test from kasiduid/next-shield')
   useEffect(() => {
     if (!isAuth && !isLoading && pathIsPrivate) replace(loginRoute)
     if (isAuth && !isLoading && pathIsPublic) replace(access)
